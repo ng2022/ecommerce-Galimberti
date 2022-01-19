9624d069ee54f4fc3a4bbcb8d16961bbc41eedaa
ng2022 <n.galimberti@gmail.com>
2022-01-19 20:42:59 +0100

    Primer desafio ecommerce Niccolo Galimberti

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Niccolò Galimberti
+        </p>
+        <p>
+          E-commerce
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
